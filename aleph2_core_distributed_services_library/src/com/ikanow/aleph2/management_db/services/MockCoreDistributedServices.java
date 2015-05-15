a05aa896e4aa12d35ad602150f6abeb3a1b7ccfb
Alex Piggott <apiggott@ikanow.com>
2015-05-15 16:40:32 -0400

    ALEPH-3 #comment unification merge

--- aleph2_core_distributed_services_library/src/com/ikanow/aleph2/management_db/services/MockCoreDistributedServices.java
+++ aleph2_core_distributed_services_library/src/com/ikanow/aleph2/management_db/services/MockCoreDistributedServices.java
@@ -3,14 +3,22 @@ package com.ikanow.aleph2.management_db.services;
 import org.apache.curator.framework.CuratorFramework;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import com.google.inject.Inject;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.ICoreDistributedServices;
 
-/** Implementation class for full Curator service
+/** Implementation class for standalone Curator instance
  * @author acp
  *
  */
-public class CoreDistributedServices implements ICoreDistributedServices {
+public class MockCoreDistributedServices implements ICoreDistributedServices {
 
+	/** Guice-invoked constructor
+	 */
+	@Inject
+	public MockCoreDistributedServices() {
+		
+	}
+	
 	 
 	/** Returns a connection to the Curator server
 	 * @return
--- aleph2_management_db_service/src/com/ikanow/aleph2/management_db/services/ManagementDbActorContext.java
+++ aleph2_management_db_service/src/com/ikanow/aleph2/management_db/services/ManagementDbActorContext.java
@@ -1,5 +1,10 @@
 package com.ikanow.aleph2.management_db.services;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import com.google.inject.Inject;
+import com.ikanow.aleph2.data_model.interfaces.shared_services.ICoreDistributedServices;
+
 import akka.actor.ActorSystem;
 
 /** Possibly temporary class to provide minimal actor context, pending moving to Guice
@@ -8,30 +13,42 @@ import akka.actor.ActorSystem;
 public class ManagementDbActorContext {
 
 	
-	/** Creates a new actor context (should only be called once_
+	/** Creates a new actor context
 	 */
-	protected ManagementDbActorContext() {
-		_actor_system = ActorSystem.create("com.ikanow.aleph2.management_db.services.CoreManagementDbService");
+	@Inject
+	protected ManagementDbActorContext(ICoreDistributedServices distributed_services) {
+		_distributed_services = distributed_services;
+		if (null == _actor_system) {
+			_actor_system = ActorSystem.create("com.ikanow.aleph2.management_db.services.CoreManagementDbService");
+		}
+		_singleton = this;
 	}
 
 	/** Returns the global actor system for the core management db service
 	 * @return the actor system
 	 */
+	@NonNull
 	public ActorSystem getActorSystem() {
 		return _actor_system;
+	}
+	
+	/** Returns the various distributed services present 
+	 * @return the distributed services
+	 */
+	@NonNull
+	public ICoreDistributedServices getDistributedServices() {
+		return _distributed_services;
 	}
 	
 	/** Gets the actor context
 	 * @return the actor context
 	 */
 	public static ManagementDbActorContext get() {
-		if (null == _singleton) {
-			_singleton = new ManagementDbActorContext();
-		}
+		// (This will only not be set if guice injection has failed, in which case there are deeper problems...)
 		return _singleton;		
 	}
 	
-	protected final ActorSystem _actor_system;
+	protected static ActorSystem _actor_system;
 	protected static ManagementDbActorContext _singleton = null;
-	
+	protected final ICoreDistributedServices _distributed_services;
 }
