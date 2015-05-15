4bf83ecc44182815282dde9dcbb95d1c88162c41
jfreydank <jfreydank@ikanow.com>
2015-05-15 09:46:38 -0500

    Added Main class and guice module.

---
+++ aleph2_data_import_manager/src/com/ikanow/aleph2/data_import_manager/module/DataImportManagerModule.java
@@ -0,0 +1,13 @@
+package com.ikanow.aleph2.data_import_manager.module;
+
+import com.google.inject.AbstractModule;
+
+public class DataImportManagerModule extends AbstractModule{
+
+	@Override
+	protected void configure() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
