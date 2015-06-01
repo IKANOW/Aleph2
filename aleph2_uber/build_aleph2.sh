79f2b4b11164adbdeb1555f2017f8091171f74a8
jfreydank <jfreydank@ikanow.com>
2015-06-01 12:03:12 -0500

    Added contrib shaded build

--- aleph2_uber/build_aleph2.sh
+++ aleph2_uber/build_aleph2.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash 
+# Build file for building all aleph2 core and contrib, the order is important here.
+# create a link in ~/bin to this file e.g. ln -s ~/git/Aleph2/aleph2_uber/build_aleph2.sh ~/bin/build_aleph2.sh
 cd ~/git/Aleph2/aleph2_core_distributed_services_library
 mvn -e install -DskipTests=true -Dmaven.compiler.verbose=true
 cd ~/git/Aleph2/aleph2_data_model
@@ -7,8 +9,12 @@ cd ~/git/Aleph2-contrib
 mvn -e install -DskipTests=true -Dmaven.compiler.verbose=true
 cd ~/git/Aleph2
 mvn -e install -DskipTests=true -Dmaven.compiler.verbose=true
+
 # build all single uber jars but using provided aleph2 modules,, so we only inlcude each modules aleph2 code
 cd ~/git/Aleph2
 mvn -e package -DskipTests=true -Daleph2.scope=provided
+
+cd ~/git/Aleph2-contrib
+mvn -e install -DskipTests=true -Daleph2.scope=provided
 
 
