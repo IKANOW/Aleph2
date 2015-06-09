378a1c2ad63929174208de6555c2dc5462eb2d3b
jfreydank <jfreydank@ikanow.com>
2015-06-09 10:55:30 -0500

    Added creation of temp file, info messages for beBucketActor and bugfixes.

--- aleph2_data_import_manager/test/com/ikanow/aleph2/data_import_manager/batch_enrichment/services/DataImportManagerTest.java
+++ aleph2_data_import_manager/test/com/ikanow/aleph2/data_import_manager/batch_enrichment/services/DataImportManagerTest.java
@@ -3,10 +3,12 @@ package com.ikanow.aleph2.data_import_manager.batch_enrichment.services;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-import java.io.File;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.apache.hadoop.fs.FileContext;
 import org.apache.log4j.Logger;
@@ -26,7 +28,14 @@ import com.ikanow.aleph2.data_import_manager.batch_enrichment.module.DataImportM
 import com.ikanow.aleph2.data_import_manager.services.DataImportActorContext;
 import com.ikanow.aleph2.data_import_manager.services.GeneralInformationService;
 import com.ikanow.aleph2.data_import_manager.utils.DirUtils;
+import com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService;
+import com.ikanow.aleph2.data_model.interfaces.shared_services.IManagementCrudService;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.IServiceContext;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean;
+import com.ikanow.aleph2.data_model.objects.data_import.EnrichmentControlMetadataBean;
+import com.ikanow.aleph2.data_model.utils.BeanTemplateUtils;
+import com.ikanow.aleph2.data_model.utils.CrudUtils;
+import com.ikanow.aleph2.data_model.utils.CrudUtils.SingleQueryComponent;
 import com.ikanow.aleph2.data_model.utils.ModuleUtils;
 import com.ikanow.aleph2.management_db.utils.ActorUtils;
 import com.typesafe.config.Config;
@@ -41,20 +50,23 @@ public class DataImportManagerTest {
 	
 	protected DataImportActorContext _actor_context;
 	//protected ManagementDbActorContext _db_actor_context;
-	
+
 	protected Config config = null;
 	protected DataImportManager dataImportManager = null;
 	
 	protected String bucketPath1 = null;
+	protected String bucketReadyPath1 = null;
 	protected String buckeFullName1= "/misc/bucket1";
 	
+	protected IManagementDbService _management_db;
+
 	@Before
 	public void setupDependencies() throws Exception {
 		if (null != _service_context) {
 			return;
 		}
 		
-		final String temp_dir = System.getProperty("java.io.tmpdir") + File.separator;
+		final String temp_dir = System.getProperty("java.io.tmpdir");
 		
 		// OK we're going to use guice, it was too painful doing this by hand...				
 		config = ConfigFactory.parseReader(new InputStreamReader(this.getClass().getResourceAsStream("/test_data_import_manager.properties")))
@@ -72,14 +84,24 @@ public class DataImportManagerTest {
 		Injector serverInjector = ModuleUtils.createInjector(Arrays.asList(new DataImportManagerModule()), Optional.of(config));
 		this.dataImportManager = serverInjector.getInstance(DataImportManager.class);
 
+		createFolderStructure();
+		this._management_db = _actor_context.getServiceContext().getCoreManagementDbService();
+		
+}
+	
+	protected void createFolderStructure(){
 		// create folder structure if it does not exist for testing.		
 		FileContext fileContext = _service_context.getStorageService().getUnderlyingPlatformDriver(FileContext.class,Optional.empty()).get();
 		logger.info("Root dir:"+_actor_context.getGlobalProperties().distributed_root_dir());
-		this.bucketPath1 = _actor_context.getGlobalProperties().distributed_root_dir()+"/data/misc/bucket1/managed_bucket/import/ready";
-		DirUtils.createDirectory(fileContext,bucketPath1);
+		this.bucketPath1 = _actor_context.getGlobalProperties().distributed_root_dir()+"/data/misc/bucket1";
+		this.bucketReadyPath1 = bucketPath1+"/managed_bucket/import/ready";
+		DirUtils.createDirectory(fileContext,bucketReadyPath1);
 		DirUtils.createDirectory(fileContext,_actor_context.getGlobalProperties().distributed_root_dir()+"/data/misc/bucket2/managed_bucket/import/ready");
 		DirUtils.createDirectory(fileContext,_actor_context.getGlobalProperties().distributed_root_dir()+"/data/misc/bucket3/managed_bucket/import/ready");
-}
+		StringBuffer sb = new StringBuffer();
+		sb.append("bucket1data\r\n");
+		DirUtils.createUTF8File(fileContext,bucketReadyPath1+"/bucket1data.txt", sb);
+	}
 	
 	@Test
 	@Ignore
@@ -112,12 +134,27 @@ public class DataImportManagerTest {
 			Props props = Props.create(BeBucketActor.class,_service_context.getStorageService());
 			ActorSystem system = _actor_context.getActorSystem();
 		    ActorRef beBucketActor = system.actorOf(props,"beBucket1");		    
+			createEnhancementBeanInDb();			
 			beBucketActor.tell(new BucketEnrichmentMessage(bucketPath1, "/misc/bucket1", ActorUtils.BATCH_ENRICHMENT_ZOOKEEPER + buckeFullName1),  ActorRef.noSender());
+			Thread.sleep(30000);
 		} catch (Exception e) {
 			logger.error("Caught exception",e);
 			fail(e.getMessage());
 		}
 	}
 	
-	
+	protected void createEnhancementBeanInDb() throws InterruptedException, ExecutionException, TimeoutException{
+
+		IManagementCrudService<DataBucketBean> dataStore = _management_db.getDataBucketStore();		
+		SingleQueryComponent<DataBucketBean> query_comp_full_name = CrudUtils.anyOf(DataBucketBean.class).when("full_name", buckeFullName1);
+			Optional<DataBucketBean> oDataBucketBean = dataStore.getObjectBySpec(query_comp_full_name).get(1, TimeUnit.SECONDS);
+			logger.debug(oDataBucketBean);
+			if(!oDataBucketBean.isPresent()){
+				EnrichmentControlMetadataBean ecm1 = BeanTemplateUtils.build(EnrichmentControlMetadataBean.class).with(EnrichmentControlMetadataBean::name,"bucketConfig1_1").with(EnrichmentControlMetadataBean::enabled, false).done().get();
+				EnrichmentControlMetadataBean ecm2 = BeanTemplateUtils.build(EnrichmentControlMetadataBean.class).with(EnrichmentControlMetadataBean::name,"bucketConfig1_2").with(EnrichmentControlMetadataBean::enabled, true).done().get();				
+				DataBucketBean bean = BeanTemplateUtils.build(DataBucketBean.class).with(DataBucketBean::_id, buckeFullName1).with(DataBucketBean::full_name, buckeFullName1).with(DataBucketBean::batch_enrichment_configs, Arrays.asList(ecm1,ecm2)).done().get();
+				dataStore.storeObject(bean);
+			}
+			
+	}
 }
