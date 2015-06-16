84ecae0d1baca24ed6eef7f22bfce7dc33bd9299
jfreydank <jfreydank@ikanow.com>
2015-06-16 17:06:26 -0500

    Added BatchEnrichment job, mapper, reducer.

---
+++ aleph2_data_import_manager/src/com/ikanow/aleph2/data_import_manager/batch_enrichment/services/mapreduce/BatchEnrichmentJob.java
@@ -0,0 +1,16 @@
+package com.ikanow.aleph2.data_import_manager.batch_enrichment.services.mapreduce;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class BatchEnrichmentJob{
+
+	public class BatchEnrichmentReducer extends Reducer<Object, Object, Object, Object> {
+
+	}
+
+	public class BatchErichmentMapper extends Mapper<Object, Object, Object, Object>{
+
+	}
+
+}
