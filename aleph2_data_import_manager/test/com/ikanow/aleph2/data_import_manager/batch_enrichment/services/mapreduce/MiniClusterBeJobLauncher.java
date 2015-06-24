286ab8bd0061205d578cc2982f8982d88842ceb5
jfreydank <jfreydank@ikanow.com>
2015-06-24 15:36:14 -0500

    Configured Added MiniYarnCluser for testing MRJobs.

--- aleph2_data_import_manager/test/com/ikanow/aleph2/data_import_manager/batch_enrichment/services/mapreduce/MiniClusterBeJobLauncher.java
+++ aleph2_data_import_manager/test/com/ikanow/aleph2/data_import_manager/batch_enrichment/services/mapreduce/MiniClusterBeJobLauncher.java
@@ -5,9 +5,12 @@ import org.apache.hadoop.conf.Configuration;
 //import org.apache.hadoop.mapred.JobConf;
 //import org.apache.hadoop.mapred.MiniMRCluster;
 import org.apache.hadoop.mapreduce.v2.MiniMRYarnCluster;
+import org.apache.hadoop.mapreduce.v2.jobhistory.JHAdminConfig;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.google.inject.Inject;
 import com.ikanow.aleph2.data_import_manager.batch_enrichment.utils.DataBucketTest;
 import com.ikanow.aleph2.data_model.objects.shared.GlobalPropertiesBean;
 
@@ -15,11 +18,12 @@ public class MiniClusterBeJobLauncher extends BeJobLauncher {
     private static final Logger logger = LogManager.getLogger(DataBucketTest.class);
 
     private MiniMRYarnCluster mrCluster;
-    
+    private 
 //    private JobConf mrClusterConf;
 //    private FileSystem localFileSystem;
 
     
+	@Inject 
 	MiniClusterBeJobLauncher(GlobalPropertiesBean globals) {
 		super(globals);
 		// TODO Auto-generated constructor stub
@@ -28,17 +32,27 @@ public class MiniClusterBeJobLauncher extends BeJobLauncher {
 
 	@Override
 	protected Configuration getConfiguration() {// TODO Auto-generated method stub
+		if(configuration == null){
+
 		//super.getConfiguration();
-		this.configuration = new Configuration(false);
+		this.configuration = new Configuration(true);
+		String stagingdir = configuration.get("yarn.app.mapreduce.am.staging-dir");
+		logger.debug("staging dir:"+stagingdir);
+		configuration.setBoolean(YarnConfiguration.YARN_MINICLUSTER_FIXED_PORTS,true);
+		configuration.setBoolean(JHAdminConfig.MR_HISTORY_MINICLUSTER_FIXED_PORTS,true);
+		//configuration.set(YarnConfiguration.RM_ADDRESS,MiniMRYarnCluster.getHostname() + ":" + this.rmPort);
+		//configuration.set(JHAdminConfig.MR_HISTORY_ADDRESS,MiniMRYarnCluster.getHostname() + ":" + this.jhsPort);
+	    
 		try {			
         //localFileSystem = FileSystem.get(configuration);		
         //mrCluster = new MiniMRCluster(1, localFileSystem.getUri().toString(), 1, null, null, new JobConf(configuration));
         mrCluster = new MiniMRYarnCluster("StandaloneTest",1);
-        mrCluster.init(configuration);                       
+        mrCluster.init(configuration);                      
+        start();
 		} catch (Exception e) {
 			logger.error("getConfiguration caused exception",e);
 		}
-        
+		}
         return configuration;
 	}
 
