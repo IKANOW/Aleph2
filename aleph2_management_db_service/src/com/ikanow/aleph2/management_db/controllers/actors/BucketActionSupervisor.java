b4d0e8e92d40467102f7b2d5f8fee5e1aaea59e1
Alex-Ikanow <apiggott@ikanow.com>
2015-06-30 23:12:35 -0400

    ALEPH-10 Started plumbing in streaming enrichment control logic - testing of joint streaming/harvest action supervisor

--- aleph2_management_db_service/src/com/ikanow/aleph2/management_db/controllers/actors/BucketActionSupervisor.java
+++ aleph2_management_db_service/src/com/ikanow/aleph2/management_db/controllers/actors/BucketActionSupervisor.java
@@ -20,6 +20,12 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.ikanow.aleph2.data_model.objects.shared.BasicMessageBean;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean.MasterEnrichmentType;
+import com.ikanow.aleph2.data_model.utils.Lambdas;
 import com.ikanow.aleph2.distributed_services.utils.AkkaFutureUtils;
 import com.ikanow.aleph2.management_db.data_model.BucketActionMessage;
 import com.ikanow.aleph2.management_db.data_model.BucketActionReplyMessage;
@@ -39,9 +45,6 @@ import akka.pattern.Patterns;
  */
 public class BucketActionSupervisor extends UntypedActor {
 
-	//TODO (ALEPH-10): First sends a message to streaming enrichment (if enabled), only send to buckets if that returns success
-	//TODO: only thing is that I don't want to add the streaming enrichment "id" to the others, right?
-	
 	//TODO (ALEPH-19): Need a scheduled thread that runs through the retries and checks each one
 	
 	//TODO (ALEPH-19): Need different timeouts to handle "found harvester it's taking its time" vs "waiting internally for harvesters"
@@ -55,16 +58,19 @@ public class BucketActionSupervisor extends UntypedActor {
 		protected RequestMessage(
 				final Class<? extends Actor> actor_type,
 				final BucketActionMessage message,
+				final String message_type,
 				final Optional<FiniteDuration> timeout)
 		{
 			this.actor_type = actor_type;
 			this.message = message;
+			this.message_type = message_type;
 			this.timeout = timeout;
 		}
 
 		protected final Class<? extends Actor> actor_type;
 		protected final BucketActionMessage message;
 		protected final Optional<FiniteDuration> timeout;
+		protected final String message_type; // ActorUtils.BUCKET_ACTION_ZOOKEEPER or STREAMING_ENRICHMENT_ZOOKEEPER 
 	}
 	
 	/** Send an action message to the multi-node distribution actor, get a future containing the reply 
@@ -78,16 +84,7 @@ public class BucketActionSupervisor extends UntypedActor {
 						final BucketActionMessage message, 
 						final Optional<FiniteDuration> timeout)
 	{
-		if (null == message.bucket().harvest_technology_name_or_id()) {
-			// Centralized check: if the harvest_technology_name_or_id isnt' present, nobody cares so short cut actually checking
-			return CompletableFuture.completedFuture(
-					new BucketActionReplyMessage.BucketActionCollectedRepliesMessage(
-							Collections.emptyList(), Collections.emptySet()
-							));
-		}
-		RequestMessage m = new RequestMessage(BucketActionDistributionActor.class, message, timeout);
-		//(the 2* ensures that it is "always" normally the bucket that times out, which is more controlled)
-		return AkkaFutureUtils.efficientWrap(Patterns.ask(supervisor, m, 2*timeout.orElse(DEFAULT_TIMEOUT).toMillis()), actor_context.dispatcher());
+		return controlLogic(supervisor, actor_context, message, BucketActionDistributionActor.class, timeout);
 	}
 	
 	/** Send an action message to the multi-node distribution actor, get a future containing the reply 
@@ -101,26 +98,17 @@ public class BucketActionSupervisor extends UntypedActor {
 					final BucketActionMessage message, 
 					final Optional<FiniteDuration> timeout)
 	{
-		if (null == message.bucket().harvest_technology_name_or_id()) {
-			// Centralized check: if the harvest_technology_name_or_id isnt' present, nobody cares so short cut actually checking
-			return CompletableFuture.completedFuture(
-					new BucketActionReplyMessage.BucketActionCollectedRepliesMessage(
-							Collections.emptyList(), Collections.emptySet()
-							));
-		}
-		RequestMessage m = new RequestMessage(BucketActionChooseActor.class, message, timeout);
-		//(the 5* ensures that it is "always" normally the bucket that times out, which is more controlled) (5* because there's an additional send-ack compared to choose actor)
-		return AkkaFutureUtils.efficientWrap(Patterns.ask(supervisor, m, 5*timeout.orElse(DEFAULT_TIMEOUT).toMillis()), actor_context.dispatcher());
-			//(choose has longer timeout because of retries)
+		return controlLogic(supervisor, actor_context, message, BucketActionChooseActor.class, timeout);
 	}
 	/* (non-Javadoc)
 	 * @see akka.actor.UntypedActor#onReceive(java.lang.Object)
 	 */
 	@Override
-	public void onReceive(Object untyped_message) throws Exception {
+	public void onReceive(final Object untyped_message) throws Exception {
 		if (untyped_message instanceof RequestMessage) {
 			RequestMessage message = (RequestMessage) untyped_message;
-			ActorRef new_child = this.context().actorOf(Props.create(message.actor_type, message.timeout, ActorUtils.BUCKET_ACTION_ZOOKEEPER));
+
+			ActorRef new_child = this.context().actorOf(Props.create(message.actor_type, message.timeout, message.message_type));
 
 			new_child.forward(message.message, this.context());
 		}
@@ -128,4 +116,136 @@ public class BucketActionSupervisor extends UntypedActor {
 			this.unhandled(untyped_message);
 		}
 	}
+	
+	/** Control logic to handle either bucket type and streaming?/harvest?
+	 * @param supervisor
+	 * @param actor_context
+	 * @param message
+	 * @param actor_type
+	 * @param timeout
+	 * @return
+	 */
+	public static CompletableFuture<BucketActionReplyMessage.BucketActionCollectedRepliesMessage> controlLogic(
+			final ActorRef supervisor, final ActorSystem actor_context,
+			final BucketActionMessage message, final Class<? extends Actor> actor_type,
+			final Optional<FiniteDuration> timeout)
+	{
+		final DataBucketBean bucket = message.bucket();
+		final boolean is_streaming = isStreaming(bucket);
+		final boolean has_harvester = hasHarvester(bucket);
+		
+		if (!is_streaming && !has_harvester) {
+			// Centralized check: if the harvest_technology_name_or_id isnt' present, nobody cares so short cut actually checking
+			return CompletableFuture.completedFuture(
+					new BucketActionReplyMessage.BucketActionCollectedRepliesMessage(
+							Collections.emptyList(), Collections.emptySet()
+							));
+		}
+		else {
+			return Lambdas.<Object, CompletableFuture<BucketActionReplyMessage.BucketActionCollectedRepliesMessage>>wrap_u(__ -> {
+				if (is_streaming) { // (streaming + ??)
+					/**/
+					System.out.println("here stream+?");
+					
+					final RequestMessage m = new RequestMessage(BucketActionChooseActor.class, message, ActorUtils.STREAMING_ENRICHMENT_ZOOKEEPER, timeout);
+					return AkkaFutureUtils.efficientWrap(Patterns.ask(supervisor, m, 
+							getTimeoutMultipler(BucketActionChooseActor.class)*timeout.orElse(DEFAULT_TIMEOUT).toMillis()), actor_context.dispatcher());
+				}
+				else { // (harvest only)
+					/**/
+					System.out.println("here harvest only");
+					
+					return CompletableFuture.<BucketActionReplyMessage.BucketActionCollectedRepliesMessage>completedFuture(null);
+				}				
+			})
+			.andThen(cf -> {
+				if (has_harvester) { // (streaming + harvest) 
+					/**/
+					System.out.println("here stream+harvest");
+					
+					final RequestMessage m = new RequestMessage(actor_type, message, ActorUtils.BUCKET_ACTION_ZOOKEEPER, timeout);
+					return cf.<BucketActionReplyMessage.BucketActionCollectedRepliesMessage>thenCompose(stream -> {							
+							// Check if the stream succeeded or failed, only call if the 
+							if ((null == stream) ||
+									(!stream.replies().isEmpty() && stream.replies().get(0).success()))
+							{
+								/**/
+								System.out.println("here stream+harvest: stream succeeds or doesn't occur");
+
+								return AkkaFutureUtils.<BucketActionReplyMessage.BucketActionCollectedRepliesMessage>
+									efficientWrap(Patterns.ask(supervisor, m, 
+										getTimeoutMultipler(actor_type)*timeout.orElse(DEFAULT_TIMEOUT).toMillis()), actor_context.dispatcher())
+											.thenApply(harvest -> {
+												if (null != stream) {
+													final java.util.List<BasicMessageBean> combined_replies = ImmutableList.<BasicMessageBean>builder()
+																												.addAll(stream.replies())
+																												.addAll(harvest.replies())
+																											.build();
+					
+													final java.util.Set<String> timed_out = ImmutableSet.<String>builder()
+																									.addAll(stream.timed_out())
+																									.addAll(harvest.timed_out())
+																								.build();
+													return new BucketActionReplyMessage.BucketActionCollectedRepliesMessage(combined_replies, timed_out);
+												}
+												else {
+													return harvest;
+												}												
+											});
+							}
+							else {
+								/**/
+								System.out.println("here stream+harvest: stream fails: " + new java.util.Date());
+
+								return CompletableFuture.completedFuture(stream);
+							}
+						} );
+				}
+				else { // (streaming only)					
+					/**/
+					System.out.println("here stream only");
+					
+					return cf;
+				}
+			})
+			.apply(null);		
+		}
+	}
+	
+	///////////////////////////////////////////////
+	
+	// LOW LEVEL UTILITIES
+	
+	/** Returns whether a bucket needs extra processing to set its streaming mode up
+	 * @param bucket
+	 */
+	public static boolean isStreaming(final DataBucketBean bucket) {
+		return Optional.ofNullable(bucket.master_enrichment_type())
+						.map(type -> (type == MasterEnrichmentType.streaming) || (type == MasterEnrichmentType.streaming_and_batch))
+						.orElse(false); // (ie if null)
+	}
+	
+	/** Returns whether a bucket needs to be sent to 1+ harvest nodes 
+	 * @param bucket
+	 * @return
+	 */
+	public static boolean hasHarvester(final DataBucketBean bucket) {
+		return (null != bucket.harvest_technology_name_or_id());
+	}
+	
+	/** Extra multiplier of default timeout, to ensure that it is "always" (normally) the actor that times out, which is more controlled
+	 *  Choose Actors are more painful because there's an extra send-ack, so use 5* instead of 2*
+	 * @param actor_type
+	 * @return
+	 */
+	public static int getTimeoutMultipler(final Class<? extends Actor> actor_type) {
+		if (BucketActionDistributionActor.class.isAssignableFrom(actor_type)) {
+			return 2;
+		}
+		if (BucketActionChooseActor.class.isAssignableFrom(actor_type)) {
+			return 5;
+		}
+		else return 1;
+	}
+	
 }
---
+++ aleph2_management_db_service/test/com/ikanow/aleph2/management_db/controllers/actors/TestBucketActionSupervisor.java
@@ -0,0 +1,380 @@
+/*******************************************************************************
+* Copyright 2015, The IKANOW Open Source Project.
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License, version 3,
+* as published by the Free Software Foundation.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+package com.ikanow.aleph2.management_db.controllers.actors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+
+import com.ikanow.aleph2.data_model.interfaces.shared_services.MockServiceContext;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean;
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean.MasterEnrichmentType;
+import com.ikanow.aleph2.data_model.objects.shared.BasicMessageBean;
+import com.ikanow.aleph2.data_model.utils.BeanTemplateUtils;
+import com.ikanow.aleph2.data_model.utils.UuidUtils;
+import com.ikanow.aleph2.distributed_services.services.ICoreDistributedServices;
+import com.ikanow.aleph2.distributed_services.services.MockCoreDistributedServices;
+import com.ikanow.aleph2.management_db.data_model.BucketActionMessage;
+import com.ikanow.aleph2.management_db.data_model.BucketActionReplyMessage;
+import com.ikanow.aleph2.management_db.data_model.BucketActionMessage.NewBucketActionMessage;
+import com.ikanow.aleph2.management_db.data_model.BucketActionReplyMessage.BucketActionCollectedRepliesMessage;
+import com.ikanow.aleph2.management_db.services.ManagementDbActorContext;
+import com.ikanow.aleph2.management_db.utils.ActorUtils;
+
+/** Tests the control logic surrounding combining streaming enrichment control with harvest control 
+ * @author Alex
+ */
+public class TestBucketActionSupervisor {
+	public static final Logger _logger = LogManager.getLogger(TestBucketActionChooseActor.class);
+	
+	//////////////////////////////////////////////////////////////////////////
+	
+	// SETUP
+	
+	// Test actors:
+	// This one always refuses
+	public static class TestActor_Refuser extends UntypedActor {
+		public TestActor_Refuser(String uuid) {
+			this.uuid = uuid;
+		}
+		private final String uuid;
+		@Override
+		public void onReceive(Object arg0) throws Exception {
+			_logger.info("Refuse from: " + uuid);
+			
+			this.sender().tell(new BucketActionReplyMessage.BucketActionIgnoredMessage(uuid), this.self());
+		}		
+	}
+
+	// This one always accepts, and returns a message
+	public static class TestActor_Accepter extends UntypedActor {
+		public TestActor_Accepter(String uuid) {
+			this.uuid = uuid;
+		}
+		private final String uuid;
+		@Override
+		public void onReceive(Object arg0) throws Exception {
+			if (arg0 instanceof BucketActionMessage.BucketActionOfferMessage) {
+				_logger.info("Accept OFFER from: " + uuid);
+				this.sender().tell(new BucketActionReplyMessage.BucketActionWillAcceptMessage(uuid), this.self());
+			}
+			else {
+				_logger.info("Accept MESSAGE from: " + uuid);
+				this.sender().tell(
+						new BucketActionReplyMessage.BucketActionHandlerMessage(uuid, 
+								new BasicMessageBean(
+										new Date(),
+										true,
+										uuid + "replaceme", // (this gets replaced by the bucket)
+										arg0.getClass().getSimpleName(),
+										null,
+										"handled",
+										null									
+										)),
+						this.self());
+			}
+		}		
+	}
+	
+	// This one always accepts, but then does nothing when it comes down to it...
+	public static class TestActor_Accepter_Refuser extends UntypedActor {
+		public TestActor_Accepter_Refuser(String uuid) {
+			this.uuid = uuid;
+		}
+		private final String uuid;
+		@Override
+		public void onReceive(Object arg0) throws Exception {
+			if (arg0 instanceof BucketActionMessage.BucketActionOfferMessage) {
+				_logger.info("Accept OFFER from: " + uuid);
+				this.sender().tell(new BucketActionReplyMessage.BucketActionWillAcceptMessage(uuid), this.self());
+			}
+			else {
+				_logger.info("Refuse MESSAGE from: " + uuid);
+				this.sender().tell(new BucketActionReplyMessage.BucketActionIgnoredMessage(uuid), this.self());
+			}
+		}		
+	}	
+
+	// This one always accepts, but then errors when it comes down to it...
+	public static class TestActor_Accepter_Error extends UntypedActor {
+		public TestActor_Accepter_Error(String uuid) {
+			this.uuid = uuid;
+		}
+		private final String uuid;
+		@Override
+		public void onReceive(Object arg0) throws Exception {
+			if (arg0 instanceof BucketActionMessage.BucketActionOfferMessage) {
+				_logger.info("Accept OFFER from: " + uuid);
+				this.sender().tell(new BucketActionReplyMessage.BucketActionWillAcceptMessage(uuid), this.self());
+			}
+			else {
+				_logger.info("Error MESSAGE from: " + uuid);
+				this.sender().tell(new BucketActionReplyMessage.BucketActionHandlerMessage(uuid, new BasicMessageBean(new Date(), false, uuid, "error", null, "error", null)), this.self()); 
+			}
+		}		
+	}	
+	
+	// This one always accepts, but then refuses when it comes down to it...
+	public static class TestActor_Accepter_Timeouter extends UntypedActor {
+		public TestActor_Accepter_Timeouter(String uuid) {
+			this.uuid = uuid;
+		}
+		private final String uuid;
+		@Override
+		public void onReceive(Object arg0) throws Exception {
+			if (arg0 instanceof BucketActionMessage.BucketActionOfferMessage) {
+				_logger.info("Accept OFFER from: " + uuid);
+				this.sender().tell(new BucketActionReplyMessage.BucketActionWillAcceptMessage(uuid), this.self());
+			}
+			else {
+				_logger.info("Timeout on MESSAGE from: " + uuid);
+			}
+		}		
+	}
+	
+	@Before
+	public void testSetup() throws Exception {
+		MockServiceContext mock_service_context = new MockServiceContext();
+		MockCoreDistributedServices mock_core_distributed_services = new MockCoreDistributedServices();
+		mock_service_context.addService(ICoreDistributedServices.class, Optional.empty(), mock_core_distributed_services);
+		
+		@SuppressWarnings("unused")
+		ManagementDbActorContext singleton = new ManagementDbActorContext(mock_service_context);		
+	}
+	
+	public DataBucketBean getBucket(String id, boolean stream, boolean harvest) {
+		return Optional.of(BeanTemplateUtils.build(DataBucketBean.class))
+					.map(b -> b.with(DataBucketBean::_id, id))
+					.map(b -> stream ? (b.with(DataBucketBean::master_enrichment_type, MasterEnrichmentType.streaming)) : b)
+					.map(b -> harvest ? (b.with(DataBucketBean::harvest_technology_name_or_id, "test")) : b)
+					.get().done().get()
+					;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////
+	
+	// TEST LOGIC
+	
+	// 1) Needs to stream+harvest, and streaming fails - check never calls harvest
+	
+	// 2) Stream only, and streaming fails (no replies) - check error
+	
+	// 3) Stream only, streaming succeeds
+	
+	// 4) Stream+harvest, streaming+harvest succeed
+	
+	// 5) Stream+harvest, streaming succeeds/harvest fails
+	
+	//1) "Needs to stream+harvest, and streaming fails - check never calls harvest"
+	// 1a - streaming fails with no replies
+	@Test
+	public void test_needsStreaming_streamingFails_noReplies() throws Exception {
+		
+		// Create a failing stream actor and a succeeding harvest actor
+		
+		// Failing stream actors
+		for (int i = 0; i < 5; ++i) {
+			String uuid = UuidUtils.get().getRandomUuid();
+			ManagementDbActorContext.get().getDistributedServices()
+				.getCuratorFramework().create().creatingParentsIfNeeded()
+				.forPath(ActorUtils.STREAMING_ENRICHMENT_ZOOKEEPER + "/" + uuid);
+			
+			ActorRef handler = ManagementDbActorContext.get().getActorSystem().actorOf(Props.create(TestActor_Refuser.class, uuid), uuid);
+			ManagementDbActorContext.get().getStreamingEnrichmentMessageBus().subscribe(handler, ActorUtils.STREAMING_ENRICHMENT_EVENT_BUS);
+		}
+		// Succeeding harvest actors
+		final HashSet<String> uuids = new HashSet<String>();		
+		for (int i = 0; i < 5; ++i) {
+			String uuid = UuidUtils.get().getRandomUuid();
+			uuids.add(uuid);
+			ManagementDbActorContext.get().getDistributedServices()
+				.getCuratorFramework().create().creatingParentsIfNeeded()
+				.forPath(ActorUtils.BUCKET_ACTION_ZOOKEEPER + "/" + uuid);
+			
+			ActorRef handler = ManagementDbActorContext.get().getActorSystem().actorOf(Props.create(TestActor_Accepter.class, uuid), uuid);
+			ManagementDbActorContext.get().getBucketActionMessageBus().subscribe(handler, ActorUtils.BUCKET_ACTION_EVENT_BUS);
+		}
+		
+		// Create bucket
+		final DataBucketBean bucket = getBucket("test_needsStreaming_streamingFails", true, true);
+		
+		// Do the test
+		
+		NewBucketActionMessage test_message = new NewBucketActionMessage(bucket, false);
+		FiniteDuration timeout = Duration.create(3, TimeUnit.SECONDS);
+		
+		final long before_time = new Date().getTime();
+		
+		final CompletableFuture<BucketActionCollectedRepliesMessage> f =
+				BucketActionSupervisor.askChooseActor(
+						ManagementDbActorContext.get().getBucketActionSupervisor(), ManagementDbActorContext.get().getActorSystem(),
+						(BucketActionMessage)test_message, 
+						Optional.of(timeout));
+																
+		BucketActionCollectedRepliesMessage reply = f.get();
+		
+		final long time_elapsed = new Date().getTime() - before_time;
+		
+		assertTrue("Shouldn't have timed out in actor", time_elapsed < 1000L);
+
+		assertTrue("Shouldn't have timed out in ask", time_elapsed < 6000L);
+		
+		assertEquals((Integer)0, (Integer)reply.timed_out().size());
+		
+		assertEquals(Collections.emptyList(), reply.replies());
+	}
+	
+	//1) "Needs to stream+harvest, and streaming fails - check never calls harvest"
+	// 1b - streaming fails with an error
+	@Test
+	public void test_needsStreaming_streamingFails_error() throws Exception {
+		
+		// Create a failing stream actor and a succeeding harvest actor
+		
+		// Failing stream actors
+		for (int i = 0; i < 5; ++i) {
+			String uuid = UuidUtils.get().getRandomUuid();
+			ManagementDbActorContext.get().getDistributedServices()
+				.getCuratorFramework().create().creatingParentsIfNeeded()
+				.forPath(ActorUtils.STREAMING_ENRICHMENT_ZOOKEEPER + "/" + uuid);
+			
+			ActorRef handler = ManagementDbActorContext.get().getActorSystem().actorOf(Props.create(TestActor_Accepter_Error.class, uuid), uuid);
+			ManagementDbActorContext.get().getStreamingEnrichmentMessageBus().subscribe(handler, ActorUtils.STREAMING_ENRICHMENT_EVENT_BUS);
+		}
+		// Succeeding harvest actors
+		final HashSet<String> uuids = new HashSet<String>();		
+		for (int i = 0; i < 5; ++i) {
+			String uuid = UuidUtils.get().getRandomUuid();
+			uuids.add(uuid);
+			ManagementDbActorContext.get().getDistributedServices()
+				.getCuratorFramework().create().creatingParentsIfNeeded()
+				.forPath(ActorUtils.BUCKET_ACTION_ZOOKEEPER + "/" + uuid);
+			
+			ActorRef handler = ManagementDbActorContext.get().getActorSystem().actorOf(Props.create(TestActor_Accepter.class, uuid), uuid);
+			ManagementDbActorContext.get().getBucketActionMessageBus().subscribe(handler, ActorUtils.BUCKET_ACTION_EVENT_BUS);
+		}
+		
+		// Create bucket
+		final DataBucketBean bucket = getBucket("test_needsStreaming_streamingFails", true, true);
+		
+		// Do the test
+		
+		NewBucketActionMessage test_message = new NewBucketActionMessage(bucket, false);
+		FiniteDuration timeout = Duration.create(3, TimeUnit.SECONDS);
+		
+		final long before_time = new Date().getTime();
+		
+		final CompletableFuture<BucketActionCollectedRepliesMessage> f =
+				BucketActionSupervisor.askChooseActor(
+						ManagementDbActorContext.get().getBucketActionSupervisor(), ManagementDbActorContext.get().getActorSystem(),
+						(BucketActionMessage)test_message, 
+						Optional.of(timeout));
+																
+		BucketActionCollectedRepliesMessage reply = f.get();
+		
+		final long time_elapsed = new Date().getTime() - before_time;
+		
+		assertTrue("Shouldn't have timed out in actor", time_elapsed < 1000L);
+
+		assertTrue("Shouldn't have timed out in ask", time_elapsed < 6000L);
+		
+		assertEquals((Integer)0, (Integer)reply.timed_out().size());
+		
+		// Should contain an error
+		assertEquals(1, reply.replies().size());
+		assertEquals(false, reply.replies().get(0).success());
+	}
+
+	//1) "Needs to stream+harvest, and streaming fails - check never calls harvest"
+	// 1b - streaming fails with an error
+	@Test
+	public void test_needsStreaming_streamingFails_timeout() throws Exception {
+		
+		// Create a failing stream actor and a succeeding harvest actor
+		
+		// Failing stream actors
+		for (int i = 0; i < 2; ++i) {
+			String uuid = UuidUtils.get().getRandomUuid();
+			ManagementDbActorContext.get().getDistributedServices()
+				.getCuratorFramework().create().creatingParentsIfNeeded()
+				.forPath(ActorUtils.STREAMING_ENRICHMENT_ZOOKEEPER + "/" + uuid);
+			
+			ActorRef handler = ManagementDbActorContext.get().getActorSystem().actorOf(Props.create(TestActor_Accepter_Timeouter.class, uuid), uuid);
+			ManagementDbActorContext.get().getStreamingEnrichmentMessageBus().subscribe(handler, ActorUtils.STREAMING_ENRICHMENT_EVENT_BUS);
+		}
+		// Succeeding harvest actors
+		final HashSet<String> uuids = new HashSet<String>();		
+		for (int i = 0; i < 5; ++i) {
+			String uuid = UuidUtils.get().getRandomUuid();
+			uuids.add(uuid);
+			ManagementDbActorContext.get().getDistributedServices()
+				.getCuratorFramework().create().creatingParentsIfNeeded()
+				.forPath(ActorUtils.BUCKET_ACTION_ZOOKEEPER + "/" + uuid);
+			
+			ActorRef handler = ManagementDbActorContext.get().getActorSystem().actorOf(Props.create(TestActor_Accepter.class, uuid), uuid);
+			ManagementDbActorContext.get().getBucketActionMessageBus().subscribe(handler, ActorUtils.BUCKET_ACTION_EVENT_BUS);
+		}
+		
+		// Create bucket
+		final DataBucketBean bucket = getBucket("test_needsStreaming_streamingFails", true, true);
+		
+		// Do the test
+		
+		NewBucketActionMessage test_message = new NewBucketActionMessage(bucket, false);
+		FiniteDuration timeout = Duration.create(3, TimeUnit.SECONDS);
+		
+		final long before_time = new Date().getTime();
+		
+		final CompletableFuture<BucketActionCollectedRepliesMessage> f =
+				BucketActionSupervisor.askChooseActor(
+						ManagementDbActorContext.get().getBucketActionSupervisor(), ManagementDbActorContext.get().getActorSystem(),
+						(BucketActionMessage)test_message, 
+						Optional.of(timeout));
+																
+		BucketActionCollectedRepliesMessage reply = f.get();
+		
+		final long time_elapsed = new Date().getTime() - before_time;
+		
+		assertTrue("Should have timed out in actor", time_elapsed > 3000L);
+
+		assertTrue("Shouldn't have timed out in ask", time_elapsed < 15000L);
+		
+		// (not sure why this is 0 and not 1, but consistent with other data bucket actor tests, so it's OK I suppose..)
+		assertEquals((Integer)0, (Integer)reply.timed_out().size());
+		
+		// Should contain an error
+		assertEquals(0, reply.replies().size());
+	}
+	
+	//TODO: 2 and beyond
+}
