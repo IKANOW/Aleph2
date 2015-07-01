aecda0f62af31d717711df1a2c5c2e5d556d25f5
cburch <cburch@ikanow.com>
2015-07-01 17:38:26 -0400

    pulled out the storm-examples dep

--- aleph2_data_import_manager/test/com/ikanow/aleph2/data_import_manager/streaming_enrichment/TestStorm.java
+++ aleph2_data_import_manager/test/com/ikanow/aleph2/data_import_manager/streaming_enrichment/TestStorm.java
@@ -46,46 +46,46 @@ public class TestStorm {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void test() throws Exception {
-		TopologyBuilder builder = new TopologyBuilder();
-		builder.setSpout("spout1", new SampleWebReaderSpout("http://lifehacker.com/the-best-board-games-for-developing-valuable-real-life-1714642211"));
-		builder.setBolt("bolt1", new SampleWordParserBolt()).shuffleGrouping("spout1");
-		
-		StormControllerUtil.submitJob(storm_cluster, "test_job", null, builder.createTopology());
-		//storm_cluster.submitJob("test_job", null, builder.createTopology());
-		
-		Thread.sleep(10000);
-		
-		assertTrue(true);
-	}
-	
-	@Ignore
-	@Test
-	public void testKafkaSpout() throws Exception {
-		final String TOPIC_NAME = "TEST_KAFKA_SPOUT";
-		final String sample_jar_file = "C:/Users/Burch/Desktop/aleph2_storm_samples-0.0.1-SNAPSHOT-jar-with-dependencies.jar";
-		//STEP 1: Run the CDS kafka test to create data in the kafka queue: TEST_KAFKA_SPOUT
-		//TODO port that code over here (need to create a CDS)
-		
-		//STEP 2: create a topology and submit it to that cluster
-		TopologyBuilder builder = new TopologyBuilder();
-		BrokerHosts hosts = new ZkHosts("api001.dev.ikanow.com:2181");
-		SpoutConfig spout_config = new SpoutConfig(hosts, TOPIC_NAME, "/" + TOPIC_NAME, "1");
-		spout_config.scheme = new SchemeAsMultiScheme(new StringScheme());
-		KafkaSpout kafka_spout = new KafkaSpout(spout_config);
-		builder.setSpout("spout1", kafka_spout );
-		builder.setBolt("bolt1", new SampleKafkaBolt()).shuffleGrouping("spout1");
-		builder.setBolt("bolt2", new SampleKafkaOutputFileBolt()).shuffleGrouping("bolt1");
-		
-		String nimbus_host = "api001.dev.ikanow.com";
-		int nimbus_thrift_port = 6627;
-		String storm_thrift_transport_plugin = "backtype.storm.security.auth.SimpleTransportPlugin";
-		IStormController storm = StormControllerUtil.getRemoteStormController(nimbus_host, nimbus_thrift_port, storm_thrift_transport_plugin);
-		List<String> jars_to_merge = new ArrayList<String>();
-		jars_to_merge.add(sample_jar_file);
-		//TODO should merge in data_model
-		StormControllerUtil.submitJob(storm, "test_kafka_spout", StormControllerUtil.buildStormTopologyJar(jars_to_merge), builder.createTopology());
-	}
+//	@Test
+//	public void test() throws Exception {
+//		TopologyBuilder builder = new TopologyBuilder();
+//		builder.setSpout("spout1", new SampleWebReaderSpout("http://lifehacker.com/the-best-board-games-for-developing-valuable-real-life-1714642211"));
+//		builder.setBolt("bolt1", new SampleWordParserBolt()).shuffleGrouping("spout1");
+//		
+//		StormControllerUtil.submitJob(storm_cluster, "test_job", null, builder.createTopology());
+//		//storm_cluster.submitJob("test_job", null, builder.createTopology());
+//		
+//		Thread.sleep(10000);
+//		
+//		assertTrue(true);
+//	}
+//	
+//	@Ignore
+//	@Test
+//	public void testKafkaSpout() throws Exception {
+//		final String TOPIC_NAME = "TEST_KAFKA_SPOUT";
+//		final String sample_jar_file = "C:/Users/Burch/Desktop/aleph2_storm_samples-0.0.1-SNAPSHOT-jar-with-dependencies.jar";
+//		//STEP 1: Run the CDS kafka test to create data in the kafka queue: TEST_KAFKA_SPOUT
+//		//TODO port that code over here (need to create a CDS)
+//		
+//		//STEP 2: create a topology and submit it to that cluster
+//		TopologyBuilder builder = new TopologyBuilder();
+//		BrokerHosts hosts = new ZkHosts("api001.dev.ikanow.com:2181");
+//		SpoutConfig spout_config = new SpoutConfig(hosts, TOPIC_NAME, "/" + TOPIC_NAME, "1");
+//		spout_config.scheme = new SchemeAsMultiScheme(new StringScheme());
+//		KafkaSpout kafka_spout = new KafkaSpout(spout_config);
+//		builder.setSpout("spout1", kafka_spout );
+//		builder.setBolt("bolt1", new SampleKafkaBolt()).shuffleGrouping("spout1");
+//		builder.setBolt("bolt2", new SampleKafkaOutputFileBolt()).shuffleGrouping("bolt1");
+//		
+//		String nimbus_host = "api001.dev.ikanow.com";
+//		int nimbus_thrift_port = 6627;
+//		String storm_thrift_transport_plugin = "backtype.storm.security.auth.SimpleTransportPlugin";
+//		IStormController storm = StormControllerUtil.getRemoteStormController(nimbus_host, nimbus_thrift_port, storm_thrift_transport_plugin);
+//		List<String> jars_to_merge = new ArrayList<String>();
+//		jars_to_merge.add(sample_jar_file);
+//		//TODO should merge in data_model
+//		StormControllerUtil.submitJob(storm, "test_kafka_spout", StormControllerUtil.buildStormTopologyJar(jars_to_merge), builder.createTopology());
+//	}
 
 }
