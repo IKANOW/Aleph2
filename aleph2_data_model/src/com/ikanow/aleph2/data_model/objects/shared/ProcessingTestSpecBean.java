d48cdcf14b8bdd83b5614b39b9b1a6e17248f438
cburch <cburch@ikanow.com>
2015-08-05 19:30:58 -0400

    #comment added some additional testing fields to test spec

--- aleph2_data_model/src/com/ikanow/aleph2/data_model/objects/shared/ProcessingTestSpecBean.java
+++ aleph2_data_model/src/com/ikanow/aleph2/data_model/objects/shared/ProcessingTestSpecBean.java
@@ -25,15 +25,33 @@ public class ProcessingTestSpecBean implements Serializable {
 	private static final long serialVersionUID = -461079824203542423L;
 	protected ProcessingTestSpecBean() {}
 	
-	/** User constructor
+	/** User constructor, defaults max storage time to 24h and
+	 * overwrite existing data to true
 	 * @param requested_num_objects
 	 * @param max_run_time_secs
 	 */
 	public ProcessingTestSpecBean(final Long requested_num_objects,
 			final Long max_run_time_secs) {
+		this(requested_num_objects, max_run_time_secs, 86400L, true);
+	}
+	
+	/**
+	 * User constructor
+	 * @param requested_num_objects
+	 * @param max_run_time_secs
+	 * @param max_storage_time_secs
+	 * @param overwrite_existing_data
+	 */
+	public ProcessingTestSpecBean(final Long requested_num_objects,
+			final Long max_run_time_secs,
+			final Long max_storage_time_secs,
+			final Boolean overwrite_existing_data) {
 		this.requested_num_objects = requested_num_objects;
 		this.max_run_time_secs = max_run_time_secs;
+		this.max_storage_time_secs = max_storage_time_secs;
+		this.overwrite_existing_data = overwrite_existing_data;
 	}
+	
 	/** A requested number of documents - this might not be the precise number returned (eg for distributed processing 
 	 * @return the desired number of objects
 	 */
@@ -47,6 +65,24 @@ public class ProcessingTestSpecBean implements Serializable {
 		return max_run_time_secs;
 	}
 	
+	/**
+	 * Permission to overwrite old test data or not
+	 * @return whether to overwrite old test data or not, defaults to true
+	 */
+	public Boolean overwrite_existing_data() {
+		return overwrite_existing_data;
+	}
+	
+	/**
+	 * The max time to store test data for
+	 * @return max time to store test data for (in seconds), defaults to 86400 (24h)
+	 */
+	public Long max_storage_time_secs() {
+		return max_storage_time_secs;
+	}
+	
 	private Long requested_num_objects;
 	private Long max_run_time_secs;
+	private Long max_storage_time_secs = 86400L; //24h
+	private Boolean overwrite_existing_data = true;
 }
