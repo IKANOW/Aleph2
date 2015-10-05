600efcffa013fa0647ee349b87849a2dee5284c6
jfreydank <jfreydank@ikanow.com>
2015-10-05 14:14:10 -0500

    Aleph-60 Modified JVMSecurityManager, added runAsWrapper.

--- aleph2_core_security/src/com/ikanow/aleph2/security/service/JVMSecurityManager.java
+++ aleph2_core_security/src/com/ikanow/aleph2/security/service/JVMSecurityManager.java
@@ -14,7 +14,9 @@
  * limitations under the License.
  *******************************************************************************/
 package com.ikanow.aleph2.security.service;
+
 import java.security.Permission;
+import java.util.Arrays;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,83 +32,152 @@ import com.ikanow.aleph2.security.utils.ErrorUtils;
 //"write:<filePath having '/' replaced with ':'>"
 //"delete:<filePath having '/' replaced with ':'>"
 //"exec:<filePath having '/' replaced with ':'>"
-//"package:<packagename>]"
+//"package  is mapped as is
 // permisson is mapped as is
 
-	public class JVMSecurityManager extends SecurityManager 
-	{		private static final Logger logger = LogManager.getLogger(JVMSecurityManager.class);
+public class JVMSecurityManager extends SecurityManager {
+	private static final Logger logger = LogManager.getLogger(JVMSecurityManager.class);
 
-		protected ThreadLocal<Boolean> tlEnabled = new ThreadLocal<Boolean>();
-		protected ThreadLocal<AuthorizationBean> tlAuthBean;
-		protected ISecurityService securityService;
-		protected ThreadLocal<ISubject> tlSubject = new ThreadLocal<ISubject>();
+	protected ThreadLocal<Boolean> tlEnabled = new ThreadLocal<Boolean>();
+	protected ThreadLocal<AuthorizationBean> tlAuthBean;
+	protected ISecurityService securityService;
+	protected ThreadLocal<ISubject> tlSubject = new ThreadLocal<ISubject>();
 
-		public JVMSecurityManager(ISecurityService securityService){
-			super();
-			this.securityService = securityService;
-			tlSubject.set(securityService.loginAsSystem());
-		}
-		
-		public void setAuthBean(AuthorizationBean authBean){
-			tlAuthBean.set(authBean);
-		}
+	public JVMSecurityManager(ISecurityService securityService) {
+		super();
+		this.securityService = securityService;
+		tlSubject.set(securityService.loginAsSystem());
+	}
 
-		public void setSecureFlag(boolean isJavascript)
-		{
-			tlEnabled.set(isJavascript);
-		}
+	public void setAuthBean(AuthorizationBean authBean) {
+		tlAuthBean.set(authBean);
+	}
 
-		@Override
-		public void checkConnect(String host, int port)
-		{	
-			
-			if (isEnabled())
-			{			
-				// TODO do we want custom logic here or just add permissions
-/*				//if failed we are using our javascript security
-				//http://en.wikipedia.org/wiki/Private_network
-				//see also http://en.wikipedia.org/wiki/Reserved_IP_addresses 
-				// (note I don't block all the addresses in there, only local/local subnet ones)
-				//deny for 10.*
-				//deny for 192.186.*
-				//deny for 127.*
-				//deny for 169.253.*
-				if ( host.matches("^(10\\.|127\\.|192\\.168\\.|172\\.1[6-9]\\.|172\\.[2][0-9]\\.|172\\.3[01]\\.|169\\.254\\.).*") )
-				{
-					throw new SecurityException("Hosts: 10.*, 192.168.*, 127.*, 172.16-31,169.254.* are not allowed to be connected to");
-				}
-				*/
-				String check = "connect:"+convertHostToWildcardPermission(host)+":"+port;
-				if (!securityService.isPermitted(tlSubject.get(), check) );
-				{
-					throwSecurityException(tlSubject," connect host:"+port);
-				}
-				
-			}
-			// Always do this: so we're the union of configured restrictions+the above custom restrictions
-			super.checkConnect(host,port);		
-		}		
-		
-		protected static String convertHostToWildcardPermission(String host) {
-			if(host!=null){
-			host = host.replaceAll(".", ":");
-			}
-			return host;
-		}
+	public void setSecureFlag(boolean isJavascript) {
+		tlEnabled.set(isJavascript);
+	}
 
-		@Override
-		public synchronized void checkRead(String file) {			
-					
-			if (isEnabled())
+	@Override
+	public void checkConnect(String host, int port) {
+
+		if (isEnabled()) {
+			// TODO do we want custom logic here or just add permissions
+			/*
+			 * //if failed we are using our javascript security
+			 * //http://en.wikipedia.org/wiki/Private_network //see also
+			 * http://en.wikipedia.org/wiki/Reserved_IP_addresses // (note I
+			 * don't block all the addresses in there, only local/local subnet
+			 * ones) //deny for 10.* //deny for 192.186.* //deny for 127.*
+			 * //deny for 169.253.* if ( host.matches(
+			 * "^(10\\.|127\\.|192\\.168\\.|172\\.1[6-9]\\.|172\\.[2][0-9]\\.|172\\.3[01]\\.|169\\.254\\.).*"
+			 * ) ) { throw new SecurityException(
+			 * "Hosts: 10.*, 192.168.*, 127.*, 172.16-31,169.254.* are not allowed to be connected to"
+			 * ); }
+			 */
+			String check = "connect:" + convertHostToWildcardPermission(host) + ":" + port;
+			if (!isPermitted(check))
 			{
-				String check = "read:"+convertPathToWildcardPermission(file);
-				if(!securityService.isPermitted(tlSubject.get(), check)){
-					throwSecurityException(tlSubject,file);
-				}
+				throwSecurityException(tlSubject, " connect host:" + port);
 			}
-			super.checkRead(file);
+
 		}
-		
+		// Always do this: so we're the union of configured restrictions+the
+		// above custom restrictions
+		super.checkConnect(host, port);
+	}
+
+
+
+	@Override
+	public synchronized void checkRead(String file) {
+
+		if (isEnabled()) {
+			String check = "read:" + convertPathToWildcardPermission(file);
+			if (!securityService.isPermitted(tlSubject.get(), check)) {
+				throwSecurityException(tlSubject, file);
+			}
+		}
+		super.checkRead(file);
+	}
+
+
+	@Override
+	public void checkRead(String file, Object context) {
+		if (isEnabled()) {
+			String check = "read:" + convertPathToWildcardPermission(file);
+			if (!isPermitted( check)) {
+				throwSecurityException(tlSubject, file);
+			}
+		}
+		super.checkRead(file, context);
+	}
+
+	@Override
+	public void checkWrite(String file) {
+		if (isEnabled()) {
+			String check = "write:" + convertPathToWildcardPermission(file);
+			if (!isPermitted( check)) {
+				throwSecurityException(tlSubject, file);
+			}
+		}
+		super.checkWrite(file);
+	}
+
+	@Override
+	public void checkDelete(String file) {
+		if (isEnabled()) {
+			String check = "delete:" + convertPathToWildcardPermission(file);
+			if (!isPermitted( check)) {
+				throwSecurityException(tlSubject, file);
+			}
+		}
+		super.checkDelete(file);
+	}
+
+	@Override
+	public void checkExec(String cmd) {
+		if (isEnabled()) {
+			String check = "exec:" + convertPathToWildcardPermission(cmd);
+			if (!isPermitted( check)) {
+				throwSecurityException(tlSubject, cmd);
+			}
+		}
+		super.checkExec(cmd);
+	}
+
+	@Override
+	public void checkPackageAccess(String packageName) {
+		if (isEnabled()) {
+			String check = packageName;
+			if (!isPermitted( check)) {
+				throwSecurityException(tlSubject, packageName);
+			}
+		}
+		super.checkPackageAccess(packageName);
+	}// TESTED (by hand)
+
+	@Override
+	public void checkPermission(Permission permission) {
+		if (isEnabled()) {
+			String check = permission.getName();
+			if (!isPermitted( check)) {
+				throwSecurityException(tlSubject, check);
+			}
+		}
+		super.checkPermission(permission);
+	}// TESTED (by hand)
+
+	protected boolean isEnabled() {
+		Boolean lock = tlEnabled.get();
+		return (lock != null && lock);
+	}
+
+	protected static void throwSecurityException(ThreadLocal<ISubject> tlSubject, String message) throws SecurityException {
+		String errorMsg = ErrorUtils.get(ErrorUtils.SECURITY_ERROR, tlSubject.get().getSubject(), message);
+		logger.error(errorMsg);
+		throw new SecurityException("Read Access is not allowed");
+	}
+	
 	protected static String convertPathToWildcardPermission(String path) {
 		if (path != null) {
 			path = path.replaceAll("/", ":");
@@ -117,87 +188,23 @@ import com.ikanow.aleph2.security.utils.ErrorUtils;
 		return path;
 	}
 
-		@Override
-		public void checkRead(String file, Object context) {
-			if (isEnabled())
-			{
-				String check = "read:"+convertPathToWildcardPermission(file);
-				if(!securityService.isPermitted(tlSubject.get(), check)){
-					throwSecurityException(tlSubject,file);
-				}
-			}
-			super.checkRead(file, context);
+	protected static String convertHostToWildcardPermission(String host) {
+		if (host != null) {
+			host = host.replaceAll(".", ":");
 		}
-		
-		@Override
-		public void checkWrite(String file) {
-			if (isEnabled())
-			{
-				String check = "write:"+convertPathToWildcardPermission(file);
-				if(!securityService.isPermitted(tlSubject.get(), check)){
-					throwSecurityException(tlSubject,file);
-				}
-			}
-			super.checkWrite(file);
-		}
-		
-		@Override
-		public void checkDelete(String file) {
-			if (isEnabled())
-			{
-				String check = "delete:"+convertPathToWildcardPermission(file);
-				if(!securityService.isPermitted(tlSubject.get(), check)){
-					throwSecurityException(tlSubject,file);
-				}
-			}
-			super.checkDelete(file);
-		}
-		
-		@Override
-		public void checkExec(String cmd) {
-			if (isEnabled())
-			{
-				String check = "exec:"+convertPathToWildcardPermission(cmd);
-				if(!securityService.isPermitted(tlSubject.get(), check)){
-					throwSecurityException(tlSubject,cmd);
-				}
-			}
-			super.checkExec(cmd);
-		}	
+		return host;
+	}
 
-		@Override
-		public void checkPackageAccess(String packageName) {
-			if (isEnabled())
-			{
-				String check = packageName;
-				if(!securityService.isPermitted(tlSubject.get(), check)){
-					throwSecurityException(tlSubject,packageName);
-				}
-			}
-			super.checkPackageAccess(packageName);
-		}//TESTED (by hand)
-		
-		@Override
-		public void checkPermission(Permission permission) { 
-			if (isEnabled())
-			{
-				String check = permission.getName();
-				if(!securityService.isPermitted(tlSubject.get(), check)){
-					throwSecurityException(tlSubject,check);
-				}
-			}
-			super.checkPermission(permission);
-		}//TESTED (by hand)
-		
+	protected boolean isPermitted(String permission) {
+		AuthorizationBean a = tlAuthBean.get();
+		ISubject subject = tlSubject.get();
+		boolean permitted = false;
+		if (subject != null) {
+			securityService.runAs(subject, Arrays.asList(a.getPrincipalName()));
+			permitted = securityService.isPermitted(tlSubject.get(), permission);
+			securityService.releaseRunAs(subject);
+		}
+		return permitted;
+	}
 
-		protected boolean isEnabled(){
-			Boolean lock = tlEnabled.get();
-			return (lock != null && lock);
-		}
-		
-		protected static void throwSecurityException(ThreadLocal<ISubject> tlSubject,String message) throws SecurityException{
-			String errorMsg = ErrorUtils.get(ErrorUtils.SECURITY_ERROR,tlSubject.get().getSubject(),message); 
-			logger.error(errorMsg);
-			throw new SecurityException("Read Access is not allowed");							
-		}
 }
