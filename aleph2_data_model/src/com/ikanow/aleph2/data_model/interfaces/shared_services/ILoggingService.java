d802396813714c64973f0b9842574c526d599f3a
cburch <cburch@ikanow.com>
2016-02-29 12:34:08 -0500

    ALEPH-60 #comment recreating the logging service branch so its up to date

---
+++ aleph2_data_model/src/com/ikanow/aleph2/data_model/interfaces/shared_services/ILoggingService.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright 2016, The IKANOW Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.ikanow.aleph2.data_model.interfaces.shared_services;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.logging.log4j.Level;
+
+import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean;
+import com.ikanow.aleph2.data_model.objects.shared.BasicMessageBean;
+
+/**
+ * @author Burch
+ *
+ */
+public interface ILoggingService {
+	//user log
+	public CompletableFuture<?> log(final Level level, final DataBucketBean bucket, final BasicMessageBean message);
+	//system log - todo some day we can split these out to be 2 different interfaces to help avoid confusion to users
+	public CompletableFuture<?> systemLog(final Level level, final DataBucketBean bucket, final BasicMessageBean message);
+}
