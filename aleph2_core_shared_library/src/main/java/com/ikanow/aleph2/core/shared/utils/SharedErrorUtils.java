1d60ac8ea9c9dddb6023c0492ce79ce5d6fe5551
Alex-Ikanow <apiggott@ikanow.com>
2016-04-05 11:19:51 -0400

    ALEPH-19 Added permission text to "shared library not found" error

--- aleph2_core_shared_library/src/main/java/com/ikanow/aleph2/core/shared/utils/SharedErrorUtils.java
+++ aleph2_core_shared_library/src/main/java/com/ikanow/aleph2/core/shared/utils/SharedErrorUtils.java
@@ -20,7 +20,7 @@ package com.ikanow.aleph2.core.shared.utils;
  */
 public class SharedErrorUtils extends com.ikanow.aleph2.data_model.utils.ErrorUtils {
 
-	public static final String SHARED_LIBRARY_NAME_NOT_FOUND = "Shared library {1} not found: {0}";
+	public static final String SHARED_LIBRARY_NAME_NOT_FOUND = "Shared library {1} not found (or user does not have read permission): {0}";
 	public static final String ERROR_LOADING_CLASS = "Error loading class {1}: {0}";
 	public static final String ERROR_CLASS_NOT_SUPERCLASS = "Error: class {0} is not an implementation of {1}: this may be because you have included eg aleph2_data_model in your class - you should not include any core/contrib JARs in there.";
 	public static final String ERROR_CACHING_SHARED_LIBS = "Misc error caching shared libs for bucket {1}: {0}";
