f8e7249baaa12aa489a24f6da66834fefa9d324f
Alex-Ikanow <apiggott@ikanow.com>
2016-04-05 09:36:32 -0400

    ALEPH-19 Fixed poll frequency sending messages without taking node_affinity into account

--- aleph2_management_db_service/src/com/ikanow/aleph2/management_db/controllers/actors/BucketPollFreqSingletonActor.java
+++ aleph2_management_db_service/src/com/ikanow/aleph2/management_db/controllers/actors/BucketPollFreqSingletonActor.java
@@ -15,24 +15,26 @@
  *******************************************************************************/
 package com.ikanow.aleph2.management_db.controllers.actors;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import scala.Tuple2;
 import scala.concurrent.duration.Duration;
 import scala.concurrent.duration.FiniteDuration;
 
 import com.ikanow.aleph2.data_model.interfaces.data_services.IManagementDbService;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.ICrudService;
-import com.ikanow.aleph2.data_model.interfaces.shared_services.ICrudService.Cursor;
 import com.ikanow.aleph2.data_model.interfaces.shared_services.IServiceContext;
 import com.ikanow.aleph2.data_model.objects.data_import.DataBucketBean;
 import com.ikanow.aleph2.data_model.objects.data_import.DataBucketStatusBean;
@@ -40,6 +42,7 @@ import com.ikanow.aleph2.data_model.utils.CrudUtils;
 import com.ikanow.aleph2.data_model.utils.Optionals;
 import com.ikanow.aleph2.data_model.utils.SetOnce;
 import com.ikanow.aleph2.data_model.utils.TimeUtils;
+import com.ikanow.aleph2.data_model.utils.Tuples;
 import com.ikanow.aleph2.data_model.utils.CrudUtils.QueryComponent;
 import com.ikanow.aleph2.data_model.utils.CrudUtils.UpdateComponent;
 import com.ikanow.aleph2.management_db.data_model.BucketActionMessage.PollFreqBucketActionMessage;
@@ -102,16 +105,16 @@ public class BucketPollFreqSingletonActor extends UntypedActor {
 		//i.e. we only think it's going to be the string "Tick" sent from the scheduler we setup in the c'tor
 		setup();
 		getExpiredBuckets().thenAccept(m -> {
-			final List<DataBucketBean> buckets = StreamSupport.stream(m.spliterator(), false).collect(Collectors.toList());			
-			buckets.stream().forEach(bucket -> {
+			final List<Tuple2<DataBucketBean, DataBucketStatusBean>> buckets = StreamSupport.stream(m.spliterator(), false).collect(Collectors.toList());			
+			buckets.stream().forEach(bucket_bstatus -> {
 				//DEBUG (investigating travis issues)
 				if (_logger.isDebugEnabled()) {
-					System.out.println("Poll Expired for bucket: " + bucket.full_name() + ": " + new Date());
+					System.out.println("Poll Expired for bucket: " + bucket_bstatus._1().full_name() + ": " + new Date());
 				}
-				_logger.debug("Poll Expired for bucket: " + bucket.full_name());				
-				updateBucketNextPollTime(bucket).thenAccept(x -> {
+				_logger.debug("Poll Expired for bucket: " + bucket_bstatus._1().full_name());				
+				updateBucketNextPollTime(bucket_bstatus._1()).thenAccept(x -> {
 					_logger.debug("Sending poll message");
-					sendPollMessage(bucket);
+					sendPollMessage(bucket_bstatus);
 				}).exceptionally(t-> {
 					_logger.error("Error updating bucket next poll time: " + t.getMessage());
 					return null;
@@ -123,28 +126,29 @@ public class BucketPollFreqSingletonActor extends UntypedActor {
 		});		
 	}
 
-	private CompletableFuture<BucketActionCollectedRepliesMessage> sendPollMessage(final DataBucketBean bucket) {
+	private CompletableFuture<BucketActionCollectedRepliesMessage> sendPollMessage(final Tuple2<DataBucketBean, DataBucketStatusBean> bucket_bstatus) {		
+		final boolean multi_node_enabled = Optional.ofNullable(bucket_bstatus._1().multi_node_enabled()).orElse(false);
 		
-		//TODO: if have node affinity, should override this
-		
-		final CompletableFuture<BucketActionCollectedRepliesMessage> poll_future = BucketActionSupervisor.askBucketActionActor(Optional.empty(),
+		final CompletableFuture<BucketActionCollectedRepliesMessage> poll_future = BucketActionSupervisor.askBucketActionActor(
+				Optional.of(multi_node_enabled || !Optional.ofNullable(bucket_bstatus._2().node_affinity()).orElse(Collections.emptyList()).isEmpty()),				
 				_actor_context.getBucketActionSupervisor(), 
 				_actor_context.getActorSystem(), 
-				new PollFreqBucketActionMessage(bucket), 
+				new PollFreqBucketActionMessage(bucket_bstatus._1()), 
 				Optional.empty());			
-		_logger.debug("Sent poll message from actor for bucket: " + bucket.full_name());
+		_logger.debug("Sent poll message from actor for bucket: " + bucket_bstatus._1().full_name());
 		return poll_future;
 	}
 
-	private CompletableFuture<Cursor<DataBucketBean>> getExpiredBuckets() {
+	private CompletableFuture<Stream<Tuple2<DataBucketBean, DataBucketStatusBean>>> getExpiredBuckets() {
 		//they are buckets where suspended:false AND next_poll_date < now
 		final QueryComponent<DataBucketStatusBean> non_suspended_expired_buckets = CrudUtils.allOf(DataBucketStatusBean.class).when(DataBucketStatusBean::suspended, false).rangeBelow(DataBucketStatusBean::next_poll_date, new Date(), false);
 		//first get the status items where suspended:false and next_poll_date < now
 		return _bucket_status_crud.get().getObjectsBySpec(non_suspended_expired_buckets).thenCompose(c -> {
 			//match those to databuckets with the ids
-			Set<String> bucket_ids = Optionals.streamOf(c.iterator(), false).map(b->b._id()).collect(Collectors.toSet());			
-			final QueryComponent<DataBucketBean> matching_ids = CrudUtils.allOf(DataBucketBean.class).withAny(DataBucketBean::_id, bucket_ids);			
-			return _bucket_crud.get().getObjectsBySpec(matching_ids);
+			final Map<String, DataBucketStatusBean> bucket_ids = Optionals.streamOf(c.iterator(), false).collect(Collectors.toMap(b -> b._id(), b->b));			
+			final QueryComponent<DataBucketBean> matching_ids = CrudUtils.allOf(DataBucketBean.class).withAny(DataBucketBean::_id, bucket_ids.keySet());			
+			return _bucket_crud.get().getObjectsBySpec(matching_ids).thenApply(cc -> 
+				Optionals.streamOf(cc.iterator(), false).map(b -> Tuples._2T(b, bucket_ids.get(b._id()))).filter(t2 -> null != t2._2()));
 		});
 	}
 	
